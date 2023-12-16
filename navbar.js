1de8ebdff81ad360d33bde94d4b9495c5b13283f
naitsirhcailada <naitsirhcailada0024@gmail.com>
2023-12-16 16:15:13 +0800

    tapat

---
+++ navbar.js
@@ -0,0 +1,9 @@
+
+  function opennav() {
+    document.getElementById('mynavbar').style.width = "100%";
+    document.getElementById('open').style.display = "none";
+  }
+  function closenav(){
+    document.getElementById('mynavbar').style.width = "0";
+    document.getElementById('open').style.display = "block";
+  }
